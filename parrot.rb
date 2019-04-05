7f03201ea489ab6bc9a3e454e57c8e20d5c4d079
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-05 18:58:14 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(talk)
-  puts "#{talk}"
-  print "#{talk}"
+def parrot("Squawk!")
+  puts "Squawk!"
+  print "Squawk!"
 end
